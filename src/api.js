b4ab6ab568b2efb5d278f727cc3ed38be0bf7a40
Gangadhar Pradhan <pradhangangadhar632@gmail.com>
2026-09-10 16:42:09 +0530

    Allow deployed frontend CORS

--- src/api.js
+++ src/api.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:
-    import.meta.env.VITE_API_URL ||
-    "https://veloop-rewards-backend-0773.onrender.com/api/",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use((config) => {
