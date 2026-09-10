dcd6741ca6505aa51763bbf693e04fb1502643b4
Gangadhar Pradhan <pradhangangadhar632@gmail.com>
2026-09-10 17:46:55 +0530

    Fix production API URL

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://veloop-rewards-backend-0773.onrender.com/api/",
 });
 
 api.interceptors.request.use((config) => {
